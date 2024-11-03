50fa49fd45ca01162c576bcae79d1cba5b1eb538
RubyScarf <91581311+RubyScarf@users.noreply.github.com>
2024-11-02 22:39:30 -0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -224,7 +224,7 @@ const animationTimeline = () => {
         }, {
             scale: 1,
             rotationY: 0,
-            color: "#ff69b4",
+            color: "#3e6375",
             ease: Expo.easeOut,
         },
         0.1,
