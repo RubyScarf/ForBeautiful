fe6d33d8766245a00a415376ff1371cca436b696
RubyScarf <91581311+RubyScarf@users.noreply.github.com>
2025-01-28 10:30:11 -0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -78,7 +78,7 @@ const animationTimeline = () => {
         2, {
             visibility: "visible",
         },
-        0.05
+        0.08
     )
     .to(".fake-btn", 0.1, {
         backgroundColor: "rgb(127, 206, 248)",
